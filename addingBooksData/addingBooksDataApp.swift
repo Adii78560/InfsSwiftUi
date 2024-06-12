76e78ca8e274f4d7872bd70737e8bdc296f0597d
Adii78560 <raiaditya145@gmail.com>
2024-06-12 09:48:47 +0530

    Initial Commit

---
+++ addingBooksData/addingBooksDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  addingBooksDataApp.swift
+//  addingBooksData
+//
+//  Created by Adii I on 12/06/24.
+//
+
+import SwiftUI
+
+@main
+struct addingBooksDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
